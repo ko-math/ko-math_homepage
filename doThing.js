e998050cab88b7d79a2b3f2e3eec741273731883
ko-math <sorehakiemasu@gmail.com>
2026-03-16 20:38:33 +0900

    Update doThing.js

--- doThing.js
+++ doThing.js
@@ -57,7 +57,7 @@ async function doThing() {
 /*
 const inputDoThing =document.getElementById('doThingCount');
 inputDoThing.addEventListener("change",doThing);*/
-await doThing();
+doThing();
 
 
 // DOMが変わったら毎回
