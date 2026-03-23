3d471bd97076ac7ecf490ddc6d3bef9cb1da8451
ko-math <sorehakiemasu@gmail.com>
2026-03-23 12:07:27 +0900

    Update dialog.js

--- dialog.js
+++ dialog.js
@@ -1,12 +1,1 @@
-const detailsList = document.querySelectorAll('.dialog');
 
-detailsList.forEach(dia => {
-  const content = dia.querySelector('.content');
-  dia.addEventListener('toggle', () => {
-    if (dia.open) {
-      content.style.maxHeight = content.scrollHeight + 'px';
-    } else {
-      content.style.maxHeight = '0px';
-    }
-  });
-});
